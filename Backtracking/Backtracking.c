c9374e858037c3b22302777afded3ce1baf1c116
hyshwhy <130438663+hyshwhy@users.noreply.github.com>
2024-05-20 22:13:13 +0700

    Update Backtracking.c

--- Backtracking/Backtracking.c
+++ Backtracking/Backtracking.c
@@ -57,50 +57,31 @@ void findStart(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols, int *startRow,
     }
 }
 
-// Function to print a path on the maze
-void printPathOnMaze(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols, int path[][2], int pathLen) {
-    char tempMaze[MAX_SIZE][MAX_SIZE];
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            tempMaze[i][j] = maze[i][j];
-        }
-    }
-
-    for (int i = 0; i < pathLen; i++) {
-        if (tempMaze[path[i][0]][path[i][1]] == '.') {
-            tempMaze[path[i][0]][path[i][1]] = ' ';
-        }
-    }
-
-    printMaze(tempMaze, rows, cols);
-    printf("\n");
-}
-
-int validContent(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if(maze[i][j]!='.' && maze[i][j]!='#' && maze[i][j]!='S' && maze[i][j]!='E'){
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
 // Backtracking function to find paths from S to E
-void findPaths(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols, int x, int y, int path[][2], int pathLen, int *pathCount, int allPaths[MAX_SIZE][MAX_SIZE][2], int *allPathsLength) {
+void findPaths(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols, int x, int y, int path[][2], int pathLen, int *pathCount, int shortestPath[MAX_SIZE][MAX_SIZE][2], int *shortestPathLength, int longestPath[MAX_SIZE][MAX_SIZE][2], int *longestPathLength) {
     // If the current position is 'E', we've found a path
     if (maze[x][y] == 'E') {
         path[pathLen][0] = x;
         path[pathLen][1] = y;
         (*pathCount)++;
 
-        // Store the path
-        for (int i = 0; i <= pathLen; i++) {
-            allPaths[*pathCount - 1][i][0] = path[i][0];
-            allPaths[*pathCount - 1][i][1] = path[i][1];
+        // Store the path if it's the shortest found so far
+        if (pathLen + 1 < *shortestPathLength) {
+            *shortestPathLength = pathLen + 1;
+            for (int i = 0; i <= pathLen; i++) {
+                shortestPath[0][i][0] = path[i][0];
+                shortestPath[0][i][1] = path[i][1];
+            }
         }
-        allPathsLength[*pathCount - 1] = pathLen + 1;
+
+         // Store the path if it's the longest found so far
+        if (pathLen + 1 > *longestPathLength) {
+            *longestPathLength = pathLen + 1;
+            for (int i = 0; i <= pathLen; i++) {
+                longestPath[0][i][0] = path[i][0];
+                longestPath[0][i][1] = path[i][1];
+            }
+        }
 
         return;
     }
@@ -119,7 +100,7 @@ void findPaths(char maze[MAX_SIZE][MAX_SIZE], int rows, int cols, int x, int y, 
 
         if (newX >= 0 && newX < rows && newY >= 0 && newY < cols &&
             (maze[newX][newY] == '.' || maze[newX][newY] == 'E')) {
-            findPaths(maze, rows, cols, newX, newY, path, pathLen, pathCount, allPaths, allPathsLength);
+            findPaths(maze, rows, cols, newX, newY, path, pathLen, pathCount, shortestPath, shortestPathLength,longestPath,longestPathLength);
         }
     }
 
@@ -132,66 +113,60 @@ int main() {
     int rows, cols;
     struct timeval start, end;
 
-
     char filename[100];
     printf("Enter the maze file name: ");
     scanf("%s", filename);
+
+    // Replace "maze.txt" with the name of the file you want to read
     readMazeFromFile(filename, maze, &rows, &cols);
-     if (!validContent(maze, rows, cols)) {
-        printf("Invalid Maze\n");
-        return 1;
-    }
 
     int startRow, startCol;
-    gettimeofday(&start, NULL);
     findStart(maze, rows, cols, &startRow, &startCol);
 
     int path[MAX_SIZE * MAX_SIZE][2];
     int pathLen = 0;
     int pathCount = 0;
 
-    // Store all paths
-    int allPaths[MAX_SIZE][MAX_SIZE][2];
-    int allPathsLength[MAX_SIZE];
+    // Store the shortest path found
+    int shortestPath[MAX_SIZE][MAX_SIZE][2];
+    int shortestPathLength = MAX_SIZE * MAX_SIZE;
 
-    findPaths(maze, rows, cols, startRow, startCol, path, pathLen, &pathCount, allPaths, allPathsLength);
+     // Store the longest path found
+    int longestPath[MAX_SIZE][MAX_SIZE][2];
+    int longestPathLength = 0;
 
+    gettimeofday(&start, NULL);
+
+    findPaths(maze, rows, cols, startRow, startCol, path, pathLen, &pathCount, shortestPath, &shortestPathLength,longestPath,&longestPathLength);
+
+    char mazeTemp1[MAX_SIZE][MAX_SIZE];
+    char mazeTemp2[MAX_SIZE][MAX_SIZE];
+    readMazeFromFile(filename, mazeTemp1, &rows, &cols);
+    readMazeFromFile(filename, mazeTemp2, &rows, &cols);
     if (pathCount == 0) {
         printf("No paths found from Start to End.\n");
+
     } else {
         printf("Number of paths found: %d\n", pathCount);
-        printf("All paths:\n");
-        for (int i = 0; i < 3; i++) {
-            printf("Path %d:\n", i + 1);
-            printPathOnMaze(maze, rows, cols, allPaths[i], allPathsLength[i]);
-        }
         printf("Shortest path found:\n");
-        int shortestPathIndex = 0;
-        int shortestPathLength = allPathsLength[0];
-        for (int i = 1; i < pathCount && i<3; i++) {
-            if (allPathsLength[i] < shortestPathLength) {
-                shortestPathIndex = i;
-                shortestPathLength = allPathsLength[i];
-            }
+        for (int i = 1; i < shortestPathLength-1; i++) {
+            mazeTemp1[shortestPath[0][i][0]][shortestPath[0][i][1]] = '*';
         }
-        int longestPathIndex = 0;
-        int longestPathLength = allPathsLength[0];
-        for (int i = 1; i < pathCount; i++) {
-            if (allPathsLength[i] > longestPathLength) {
-                longestPathIndex = i;
-                longestPathLength = allPathsLength[i];
-            }
-        }
-        printPathOnMaze(maze, rows, cols, allPaths[shortestPathIndex], allPathsLength[shortestPathIndex]);
-        printf("longest path found:\n");
-        printPathOnMaze(maze, rows, cols, allPaths[longestPathIndex], allPathsLength[longestPathIndex]);
-    }
-        gettimeofday(&end, NULL);
+        printMaze(mazeTemp1, rows, cols);
 
+        printf("longest path found:\n");
+        for (int i = 1; i < longestPathLength-1; i++) {
+            mazeTemp2[longestPath[0][i][0]][longestPath[0][i][1]] = '*';
+        }
+        printMaze(mazeTemp2, rows, cols);
+    }
+
+       gettimeofday(&end, NULL);
         long seconds = end.tv_sec - start.tv_sec;
         long micros = end.tv_usec - start.tv_usec;
         double elapsed = seconds * 1e3 + micros / 1e3;
 
         printf("\nWaktu yang dibutuhkan : %.6f ms\n", elapsed);
         return 0;
+
 }
